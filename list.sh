d55c0450ee6a1bd6df3102af46706d97b3b52fe8
lanyi233 <lanyi233@qq.com>
2024-04-13 16:46:10 +0800

    water commit

--- list.sh
+++ list.sh
@@ -1,1 +1,2 @@
-find . -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.gif" -o -iname "*.svg" -o -iname "*.jpeg" -o -iname "*.webp" -o -iname "*.ico" \) | sed 's|^./||' | jq -R . | jq -s . > list.json
+# find . -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.gif" -o -iname "*.svg" -o -iname "*.jpeg" -o -iname "*.webp" -o -iname "*.ico" \) | sed 's|^./||' | jq -R . | jq -s . > list.json
+find . -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.gif" -o -iname "*.svg" -o -iname "*.jpeg" -o -iname "*.webp" -o -iname "*.ico" \) | sed 's|^./||' | sort -V | jq -R . | jq -s . > list.json
